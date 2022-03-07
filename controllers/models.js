20d1e6b8ae813166355e89b626c15034221807ba
Odhith senevirathne <odhith.sen@gmail.com>
2022-03-07 18:46:22 +0530

    Add methods to insert and retrieve 3D model data from the database

--- controllers/models.js
+++ controllers/models.js
@@ -1,24 +1,70 @@
 import Model from "../models/Model.js";
+import fs from "fs";
+import { path } from "../path.js"; // contain the absolute path of the index.js
 
-//method to get a specific user's details //remove
-export const getModel = async (req, res) => {
-  //   try {
-  //     const users = await User.find();
-  //     res.status(200).json(users);
-  //   } catch (error) {
-  //     res.status(404).json({ message: error.message });
-  //   }
+// method to get model details that belongs to a specific project(ex-http://localhost:5000/models/createModel?projectID=sample13242343423423)
+export const getModels = async (req, res) => {
+  let { projectID } = req.query;
+  console.log(projectID); //remove
+  if (typeof projectID != "undefined") {
+    try {
+      const models = await Model.find({ projectId: projectID }).exec();
+      res.status(200).json(models);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid projectID" });
+  }
 };
 
-//method to create a 3D model
+//method to create a 3D model entry and save them to the database.(ex-http://localhost:5000/models/createModel?userID=5ewerewr&projectID=sample13242343423423)
 export const createModel = async (req, res) => {
-  //How do you get a list of the names of all files present in a directory in Node.js?
-  //reffer https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j
-  //async=> https://stackoverflow.com/questions/2727167/how-do-you-get-a-list-of-the-names-of-all-files-present-in-a-directory-in-node-j/37532027#37532027
-  //   try {
-  //     const users = await User.find();
-  //     res.status(200).json(users);
-  //   } catch (error) {
-  //     res.status(404).json({ message: error.message });
-  //   }
+  try {
+    let { userID, projectID } = req.query;
+    console.log(userID, projectID); //remove
+
+    if (typeof userID != "undefined" && typeof projectID != "undefined") {
+      let projectFolder = `${path}/3Dmodels/${userID}/${projectID}`;
+      let models = [];
+
+      try {
+        // should make this file reading part async, the code is bellow
+        fs.readdirSync(projectFolder).forEach((file) => {
+          let fileName = file.split(".");
+          fileName = fileName[0];
+
+          const model = {
+            modelName: fileName,
+            modelFileLocation: `${path}/3Dmodels/${userID}/${projectID}`,
+            projectId: projectID,
+          };
+
+          try {
+            const newModel = new Model(model);
+            newModel.save();
+          } catch (error) {
+            res.status(409).json({ message: error.message });
+          }
+          models.push(model);
+        });
+
+        console.log(models); //remove
+        res.status(200).json(models); // check and remove, no need to send the array of models
+      } catch (error) {
+        res.status(404).json({ message: error.message });
+      }
+    } else {
+      res.status(400).json({ message: "Invalid userID or projectID" });
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+
+  // fs.readdir(projectFolder, (err, files) =>{
+  //   files.forEach((file) => {
+  //     console.log(file); // remove
+  //     modelNames.push(file);
+  //   });
+  // });
 };
