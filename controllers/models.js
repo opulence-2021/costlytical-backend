527a0493b9753d205552df709211bceec345be99
odhithSen <72816747+odhithSen@users.noreply.github.com>
2022-03-10 09:43:39 +0530

    Add method to update 3D model details (#10)

--- controllers/models.js
+++ controllers/models.js
@@ -2,7 +2,7 @@ import Model from "../models/Model.js";
 import fs from "fs";
 import { path } from "../path.js"; // contain the absolute path of the index.js
 
-// method to get model details that belongs to a specific project(ex-http://localhost:5000/models/createModel?projectID=sample13242343423423)
+// method to get model details that belongs to a specific project(ex-http://localhost:5000/models/createModel?projectID=sample13242343423423, and array of objects in the request body.)
 export const getModels = async (req, res) => {
   let { projectID } = req.query;
   console.log(projectID); //remove
@@ -10,6 +10,45 @@ export const getModels = async (req, res) => {
     try {
       const models = await Model.find({ projectId: projectID }).exec();
       res.status(200).json(models);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid projectID" });
+  }
+};
+
+// method to update the existing models details (quantity, layerHeight, material)(ex-http://localhost:5000/models/updateModels?projectID=sample13242343423423)
+export const updateModels = async (req, res) => {
+  let { projectID } = req.query; // check and remove
+  console.log(projectID); //remove
+  let models;
+
+  if (typeof projectID != "undefined") {
+    try {
+      models = req.body;
+      // console.log(models); //remove
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+    try {
+      for (let i = 0; i < models.length; i++) {
+        let model = models[i];
+        console.log(model.id); //remove
+        let result = await Model.updateOne(
+          { _id: model.id },
+          {
+            $set: {
+              modelQuantity: model.modelQuantity,
+              layerHeight: model.layerHeight,
+              materialName: model.materialName,
+            },
+          }
+        );
+        // console.log(result.matchedCount); //remove
+      }
+
+      res.status(200).json({ message: "Models updated successfully" });
     } catch (error) {
       res.status(404).json({ message: error.message });
     }
--- routes/models.js
+++ routes/models.js
@@ -1,11 +1,12 @@
 // module imports
 import express from "express";
 //controller imports
-import { getModels, createModel } from "../controllers/models.js";
+import { getModels, updateModels, createModel } from "../controllers/models.js";
 
 const router = express.Router();
 
 router.get("/getModels", getModels);
 router.post("/createModel", createModel);
+router.put("/updateModels", updateModels);
 
 export default router;
