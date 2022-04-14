1946157483b5917a0b9bd581d96a96c2d60bc330
Odhith senevirathne <odhith.sen@gmail.com>
2022-04-14 14:50:35 +0530

    Configure https

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import fileUpload from "express-fileupload";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import https from "https";
+import path from "path";
+import fs from "fs";
+import { certPath } from "./path.js";
 
 //route imports
 import userNameRoutes from "./routes/userName.js"; //remove
@@ -26,7 +30,7 @@ app.use("/generateCost", costGeneratorRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-//database connection URL => change to a .env_variyable later
+//database connection URL
 const CONNECTION_URL =
   "mongodb+srv://costlyticalDB:pNGZTQ68GZein2vjBa@cluster0.f35s5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 //server port
@@ -35,7 +39,16 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT} ...`))
+    ssslServer.listen(PORT, () =>
+      console.log(`Server running on port: ${PORT} ...`)
+    )
   )
   .catch((error) => console.log(error.message));
-// mongoose.set("useFindAndModify", false); //chek produce an error
+
+const ssslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(certPath, "privkey.pem")),
+    cert: fs.readFileSync(path.join(certPath, "cert.pem")),
+  },
+  app
+);
